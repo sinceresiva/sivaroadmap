f42c14f7a3504daec9249b996a3e38ad5aa0c048
SID-SHREYAS\Siva <siva.uk@outlook.com>
2017-10-13 08:01:02 +0530

    QTee first checkin

---
+++ Technical/Projects/QTee/Source_Code/QTee_Web/www/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using www.Models;
+
+namespace www.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return new FilePathResult("ng-app-review/src/index.html", "text/html");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
